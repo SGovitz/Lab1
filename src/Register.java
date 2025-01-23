f22ce25256259c8b91f602ac54cbe5e23be7763c
Shawn Govitz <SGOVITZ@cub.uca.edu>
2025-01-23 09:05:11 -0600

    Added Images

--- src/Register.java
+++ src/Register.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class Register {
     private static final Denomination[] denominations = {
-            new Denomination("Hundred Note", 100.00, "bill", "Images/hundred.jpg"),
-            new Denomination("Fifty Note", 50.00, "bill", "Images/fifty.jpg"),
-            new Denomination("Twenty Note", 20.00, "bill", "Images/twenty.jpg"),
-            new Denomination("Ten Note", 10.00, "bill", "Images/ten.jpg"),
-            new Denomination("Five Note", 5.00, "bill", "Images/five.jpg"),
-            new Denomination("One Note", 1.00, "bill", "Images/one.jpg"),
-            new Denomination("Quarter", 0.25, "coin", "Images/quarter.jpg"),
-            new Denomination("Dime", 0.10, "coin", "Images/dime.jpg"),
-            new Denomination("Nickel", 0.05, "coin", "Images/nickel.jpg"),
-            new Denomination("Penny", 0.01, "coin", "Images/penny.jpg")
+            new Denomination("Hundred Note", 100.00, "bill", "Images/Hundred_Bill.jpg"),
+            new Denomination("Fifty Note", 50.00, "bill", "Images/Fifty_Bill.jpg"),
+            new Denomination("Twenty Note", 20.00, "bill", "Images/Twenty_Bill.jpg"),
+            new Denomination("Ten Note", 10.00, "bill", "Images/Ten_Bill.jpg"),
+            new Denomination("Five Note", 5.00, "bill", "Images/Five_Bill.jpg"),
+            new Denomination("One Note", 1.00, "bill", "Images/One_Bill.jpg"),
+            new Denomination("Quarter", 0.25, "coin", "Images/Quarter_Coin.jpeg"),
+            new Denomination("Dime", 0.10, "coin", "Images/Dime_Coin.png"),
+            new Denomination("Nickel", 0.05, "coin", "Images/Nickel_Coin.jpeg"),
+            new Denomination("Penny", 0.01, "coin", "Images/Penny_Coin.png")
     };
     public Purse makeChange(double amt){
         Purse money = new Purse();
